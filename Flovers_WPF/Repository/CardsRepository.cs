15f3fa2c197f867b45c2be3e84bf4a212223bf69
KnYaZuK - Laptop <knyazuk@yandex.ru>
2015-04-24 12:30:18 +0300

    Добавлены интерфейс и контроллер для модели Cards

---
+++ Flovers_WPF/Repository/CardsRepository.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using Flovers_WPF.DataAccess;
+using Flovers_WPF.DataModel;
+
+namespace Flovers_WPF.Repository
+{
+    class CardsRepository :ICardsRepository
+    {
+        SQLiteAsyncConnection conn;
+
+        public CardsRepository(IDBConnection oIDBConnection)
+        {
+            conn = oIDBConnection.GetAsyncConnection();
+        }
+
+        public async Task Insert_Cards_Async(Cards cards)
+        {
+            await conn.InsertAsync(cards);
+        }
+
+        public async Task Update_Cards_Async(Cards cards)
+        {
+            await conn.UpdateAsync(cards);
+        }
+
+        public async Task Delete_Cards_Async(Cards cards)
+        {
+            await conn.DeleteAsync(cards);
+        }
+
+        public async Task<List<Cards>> Select_All_Cards_Async()
+        {
+            return await conn.Table<Cards>().ToListAsync();
+        }
+
+        public async Task<List<Cards>> Select_Cards_Async(string query)
+        {
+            return await conn.QueryAsync<Cards>(query);
+        }
+    }
+}
---
+++ Flovers_WPF/Repository/ICardsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flovers_WPF.DataModel;
+
+namespace Flovers_WPF.Repository
+{
+    interface ICardsRepository
+    {
+        Task Insert_Cards_Async(Cards cards);
+        Task Update_Cards_Async(Cards cards);
+        Task Delete_Cards_Async(Cards cards);
+        Task<List<Cards>> Select_All_Cards_Async();
+        Task<List<Cards>> Select_Cards_Async(string query);
+    }
+}
