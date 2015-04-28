b43c0f53ad4a5d5778d1a6b848a7b2052bcf68b6
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2015-04-28 23:54:13 +0400

    сделал регистрацию

--- Flovers_WPF/Registration.xaml.cs
+++ Flovers_WPF/Registration.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using Flovers_WPF.Repository;
+using Flovers_WPF.DataAccess;
+using Flovers_WPF.DataModel;
 
 namespace Flovers_WPF
 {
@@ -20,9 +23,43 @@ namespace Flovers_WPF
     /// </summary>
     public partial class Registration : MetroWindow
     {
+        UsersRepository oUsersRepository;
+        public bool isRegistered;
+
         public Registration()
         {
             InitializeComponent();
+        }
+
+        private async Task Initialize_Database()
+        {
+            DBConnection oDBConnection = new DBConnection();
+            await oDBConnection.InitializeDatabase();
+            oUsersRepository = new UsersRepository(oDBConnection);
+        }
+
+        public async void registration()
+        {
+            await oUsersRepository.Insert_Users_Async(new Users(lb_reg_login.Text,lb_reg_pass.Password));
+            MessageBox.Show("Вы зарегистрированы!");
+        }
+
+        private void bt_register_Click(object sender, RoutedEventArgs e)
+        {
+            if (lb_reg_login.Text == "" || lb_reg_pass.Password == "")
+            {
+                lb_alert.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lb_alert.Visibility = System.Windows.Visibility.Hidden;
+                registration();
+            }
+        }
+
+        private async void reg_form_Loaded(object sender, RoutedEventArgs e)
+        {
+            await Initialize_Database();
         }
 
     }
