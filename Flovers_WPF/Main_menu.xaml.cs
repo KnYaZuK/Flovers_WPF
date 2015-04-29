366724f4590d29107fa5c03396511ebc831d4f1f
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2015-04-29 22:52:33 +0400

    в процессе создания главного меню

--- Flovers_WPF/Main_menu.xaml.cs
+++ Flovers_WPF/Main_menu.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,7 +18,7 @@ namespace Flovers_WPF
     /// <summary>
     /// Interaction logic for Main_menu.xaml
     /// </summary>
-    public partial class Main_menu : Window
+    public partial class Main_menu : MetroWindow
     {
         public Main_menu()
         {
