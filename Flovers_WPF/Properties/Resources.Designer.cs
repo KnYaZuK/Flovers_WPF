ff30f022667597a9b0d4ac9a4408ec4bc8b02c3d
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2015-05-01 23:32:19 +0400

    удалил лишние файлы

--- Flovers_WPF/Properties/Resources.Designer.cs
+++ Flovers_WPF/Properties/Resources.Designer.cs
@@ -61,16 +61,6 @@ namespace Flovers_WPF.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap _73341529 {
-            get {
-                object obj = ResourceManager.GetObject("_73341529", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
         internal static System.Drawing.Icon controlpanel {
