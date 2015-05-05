26802dce40bc18e43c75c7f1690ba2aaf04f2e1f
KnYaZuK - Desktop <knyazuk@yandex.ru>
2015-05-05 16:38:30 +0300

    Сделал корректное отображение и биндинги. Закончил весь функционал окна.

--- Flovers_WPF/CreateFlowerWindow.xaml.cs
+++ Flovers_WPF/CreateFlowerWindow.xaml.cs
@@ -30,24 +30,33 @@ namespace Flovers_WPF
 
         struct Accessories_Flowers
         {
-            public string Type { get; set; }
-            public string Name { get; set; }
-            public decimal Count { get; set; }
-            //public Accessories accessories { get; set; }
-            //public Flowers flower { get; set; }
+            public Contents contents { get; set; } // Хранит экземпляр выделенного компонента. Необходимо для обновления и удаления.
+            public decimal count { get; set; } // Хранит количество компонента в составе
+
+            public string type { get; set; } // Хранит символьное название компонента.
+            public object component { get; set; } // Хранит компонент ( Аксессуар или Цветок ).
+
+            public int type_index { get; set; } // Хранит индекс типа компонента в комбобоксе. Необходимо для биндингов.
+            public int component_index { get; set; } // Хранит индекс компонента в комбобоксе. Необходимо для биндингов.
         }
 
-        BouquetsRepository oBouquetsRepository;
-        ContentsRepository oContentsRepository;
-        AccessoriesRepository oAccessoriesRepository;
-        FlowersRepository oFlowersRepository;
+        BouquetsRepository oBouquetsRepository;         //
+        ContentsRepository oContentsRepository;         // Контроллеры
+        AccessoriesRepository oAccessoriesRepository;   // Таблиц
+        FlowersRepository oFlowersRepository;           //
 
-        Bouquets bouquet;
-        Accessories accessories;
-        Flowers flowers;
+        Bouquets bouquet; // Храним выделенный букет.
+        object component; // Храним выделенный в комбобоксе аксессуар или цветок.
+
+        Accessories_Flowers accessories_flowers; // Храним данные для вывода их в listview компонентов
 
         SQLite.SQLiteAsyncConnection conn;
 
+        /// <summary>
+        /// Инициализация переменных при загрузке формы
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void MetroWindow_Loaded(object sender, RoutedEventArgs e)
         {
             DBConnection oDBConnection = new DBConnection();
@@ -66,6 +75,10 @@ namespace Flovers_WPF
             Clear_Control_Bouquet();
         }
 
+        /// <summary>
+        /// Обновление списка с букетами
+        /// </summary>
+        /// <returns></returns>
         private async Task Update_ListView_Bouquets()
         {
             List<Bouquets> result = await oBouquetsRepository.Select_All_Bouquets_Async();
@@ -73,29 +86,38 @@ namespace Flovers_WPF
             listview_Bouquet.ItemsSource = result;
         }
 
+        /// <summary>
+        /// Обновление списка с Компонентами. В список загружаются только компоненты выбранного букета.
+        /// </summary>
+        /// <returns></returns>
         private async Task Update_ListView_Contents()
         {
             List<Accessories_Flowers> acc_flo = new List<Accessories_Flowers>();
 
-            List<Contents> result = await oContentsRepository.Select_All_Contents_Async();
+            List<Contents> result = await oContentsRepository.Select_Contents_Async("select * from contents where contents.bouquets_id = " + bouquet.bouquets_id );
 
             foreach(var c in result)
             {
                 Accessories_Flowers af = new Accessories_Flowers();
 
+                af.contents = c; // Необходимо для реализации обновления и удаления
+                af.count = c.count; // Необходима для корректного отображения количества.
+                
                 if ( c.accessories_id != -1 )
                 {
-                    Accessories a = await conn.GetAsync<Accessories>(c.accessories_id);
-                    af.Type = "Аксессуар";
-                    af.Name = a.name;
-                    af.Count = c.count;
+                    af.component = await conn.GetAsync<Accessories>(c.accessories_id);
+                    af.type = "Аксессуар";
+                    af.type_index = 0;
+
+                    af.component_index = await Get_Component_Index( true, (af.component as Accessories).accessories_id );
                 }
                 else
                 {
-                    Flowers f = await conn.GetAsync<Flowers>(c.flowers_id);
-                    af.Type = "Цветок";
-                    af.Name = f.name;
-                    af.Count = c.count;
+                    af.component =  conn.GetAsync<Flowers>(c.flowers_id).Result;
+                    af.type = "Цветок";
+                    af.type_index = 1;
+
+                    af.component_index = await Get_Component_Index( false, (af.component as Flowers).flowers_id);
                 }
 
                 acc_flo.Add(af);
@@ -104,6 +126,53 @@ namespace Flovers_WPF
             listview_Content.ItemsSource = acc_flo;
         }
 
+        /// <summary>
+        /// Функция создаёт соответствие меж компонентом и текущим индексом аналогичного компонента в комбобоксе.
+        /// </summary>
+        /// <param name="type">true - Аксессуар, false - Цветок</param>
+        /// <param name="component_id">id соответствующего Компонента</param>
+        /// <returns>Возвращает index компонента в комбобоксе</returns>
+        private async Task<int> Get_Component_Index( bool type, int component_id )
+        {
+            int count = 0;
+
+            if ( type )
+            {
+                List<Accessories> ores = await oAccessoriesRepository.Select_All_Accessories_Async();
+
+                foreach ( var a in ores )
+                {
+                    if ( a.accessories_id == component_id )
+                    {
+                        return count;
+                    }
+
+                    count++;
+                }
+            }
+            else
+            {
+                List<Flowers> result = await oFlowersRepository.Select_All_Flowers_Async();
+
+                foreach (var f in result)
+                {
+                    if (f.flowers_id == component_id )
+                    {
+                        return count;
+                    }
+
+                    count++;
+                }
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Обновление комбобокса выбора компонента. Зависит от выбранного объекта в комбобоксе типов.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
         private async Task Update_ComboBox_Contents( bool type )
         {
             if ( type )
@@ -120,20 +189,46 @@ namespace Flovers_WPF
             }
         }
 
+        /// <summary>
+        /// Сброс интерфейса сетки букета в состояние по умолчанию.
+        /// </summary>
         private void Clear_Control_Bouquet()
         {
+            grid_Bouquet.DataContext = null;
+
+            listview_Bouquet.SelectedIndex = -1;
+            listview_Content.ItemsSource = null;
+
             button_CreateBouquet.IsEnabled = true;
             button_UpdateBouquet.IsEnabled = false;
+
             grid_Content.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Сброс интерфейса сетки компонентов в состояние по умолчанию.
+        /// </summary>
         private void Clear_Control_Content()
         {
+            grid_Content.DataContext = null;
+
+            listview_Content.SelectedIndex = -1;
+
+            combobox_Content.ItemsSource = null;
+            combobox_TypeContent.SelectedIndex = -1;
+
+            numericupdown_CountContent.Value = null;
+
             button_CreateContent.IsEnabled = true;
             button_UpdateContent.IsEnabled = false;
             button_DeleteContent.IsEnabled = false;
         }
 
+        /// <summary>
+        /// Создание нового букета.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void button_CreateBouquet_Click(object sender, RoutedEventArgs e)
         {
             await oBouquetsRepository.Insert_Bouquets_Async(new Bouquets(textbox_NameBouquet.Text, Double.Parse(textbox_PriceExtraBouquet.Text)));
@@ -143,6 +238,11 @@ namespace Flovers_WPF
             Clear_Control_Bouquet();
         }
 
+        /// <summary>
+        /// Обновление выбранного букета. Обновляются поля с Именем и Доп.Стоимостью
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void button_UpdateBouquet_Click(object sender, RoutedEventArgs e)
         {
             bouquet.name = textbox_NameBouquet.Text;
@@ -151,18 +251,25 @@ namespace Flovers_WPF
             await oBouquetsRepository.Update_Bouquets_Async(bouquet);
 
             await Update_ListView_Bouquets();
+
+            Clear_Control_Bouquet();
         }
 
+        /// <summary>
+        /// Создание компонента для выбранного букета
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void button_CreateContent_Click(object sender, RoutedEventArgs e)
         {
             if ( combobox_TypeContent.SelectedIndex == 0)
             {
-                await oContentsRepository.Insert_Contents_Async(new Contents( (decimal) numericupdown_CountContent.Value, bouquet.bouquets_id, -1, accessories.accessories_id));
+                await oContentsRepository.Insert_Contents_Async(new Contents( (decimal) numericupdown_CountContent.Value, bouquet.bouquets_id, -1, (component as Accessories).accessories_id));
             }
 
             if (combobox_TypeContent.SelectedIndex == 1)
             {
-                await oContentsRepository.Insert_Contents_Async(new Contents( (decimal) numericupdown_CountContent.Value, bouquet.bouquets_id, flowers.flowers_id, -1));
+                await oContentsRepository.Insert_Contents_Async(new Contents( (decimal) numericupdown_CountContent.Value, bouquet.bouquets_id, (component as Flowers).flowers_id, -1));
             }
 
             await Update_ListView_Contents();
@@ -171,16 +278,54 @@ namespace Flovers_WPF
 
         }
 
-        private void button_UpdateContent_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Обновление выбранного компонента у выбранного букета.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void button_UpdateContent_Click(object sender, RoutedEventArgs e)
         {
 
+            accessories_flowers.contents.count = (decimal) numericupdown_CountContent.Value;
+
+            if (combobox_TypeContent.SelectedIndex == 0)
+            {
+                accessories_flowers.contents.accessories_id = (component as Accessories).accessories_id;
+                accessories_flowers.contents.flowers_id = -1;
+            }
+
+            if (combobox_TypeContent.SelectedIndex == 1)
+            {
+                accessories_flowers.contents.flowers_id = (component as Flowers).flowers_id;
+                accessories_flowers.contents.accessories_id = -1;
+            }
+
+            await oContentsRepository.Update_Contents_Async(accessories_flowers.contents);
+
+            await Update_ListView_Contents();
+
+            Clear_Control_Content();
         }
 
-        private void button_DeleteContent_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Удаление выбранного компонента выбранного букета.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void button_DeleteContent_Click(object sender, RoutedEventArgs e)
         {
+            await oContentsRepository.Delete_Contents_Async(accessories_flowers.contents);
 
+            await Update_ListView_Contents();
+
+            Clear_Control_Content();
         }
 
+        /// <summary>
+        /// Выделение букета при нажатии ЛКМ и заполнение текстбоксов значениями этого букета. Запоминает выбранный букет.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void listview_Bouquet_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             bouquet = listview_Bouquet.SelectedItem as Bouquets;
@@ -196,32 +341,50 @@ namespace Flovers_WPF
             Clear_Control_Content();
         }
 
+        /// <summary>
+        /// Снятие выделения с букета при нажатии ПКМ и сброс интерфейса в состояние по умолчанию.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void listview_Bouquet_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-            listview_Bouquet.SelectedIndex = -1;
-
-            grid_Bouquet.DataContext = null;
-            listview_Content.ItemsSource = null;
-
             Clear_Control_Bouquet();
         }
 
+        /// <summary>
+        /// Выделение компонента при нажатии ЛКМ и заполнение текстбоксов значениями этого компонента. Запоминает выбранный компонент. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void listview_Content_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            button_CreateBouquet.IsEnabled = false;
+
+            accessories_flowers = (Accessories_Flowers) listview_Content.SelectedItem;
+
+            Clear_Control_Content();
+
+            grid_Content.DataContext = accessories_flowers;
+
+            button_CreateContent.IsEnabled = false;
             button_UpdateContent.IsEnabled = true;
             button_DeleteContent.IsEnabled = true;
         }
 
+        /// <summary>
+        /// Снятие выделения с компонента при нажатии ПКМ и сброс интерфейса в состояние по умолчанию.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void listview_Content_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
         {
-            listview_Content.SelectedIndex = -1;
-
-            grid_Content.DataContext = null;
-
             Clear_Control_Content();
         }
 
+        /// <summary>
+        /// При изменении типа компонента обновляет значения комбобокса с компонентами.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void combobox_TypeContent_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (combobox_TypeContent.SelectedIndex == 0)
@@ -235,16 +398,21 @@ namespace Flovers_WPF
             }
         }
 
+        /// <summary>
+        /// Запоминает выбранный в комбобоксе компонент.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void combobox_Content_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (combobox_TypeContent.SelectedIndex == 0)
             {
-                accessories = combobox_Content.SelectedItem as Accessories;
+                component = combobox_Content.SelectedItem as Accessories;
             }
 
             if (combobox_TypeContent.SelectedIndex == 1)
             {
-                flowers = combobox_Content.SelectedItem as Flowers;
+                component = combobox_Content.SelectedItem as Flowers;
             }
         }
     }
