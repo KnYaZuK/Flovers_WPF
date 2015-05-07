e9db107610d506202b6aa249de0b9374ef00525a
AndrewKosmos <EarthPlanetIsGreat@yandex.ru>
2015-05-07 20:28:03 +0400

    доделал окно спецпредложений, работу с вебкамерой, сделал добавление изображений

--- Flovers_WPF/Special_Deal_Window.xaml.cs
+++ Flovers_WPF/Special_Deal_Window.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Flovers_WPF
 {
@@ -23,6 +25,63 @@ namespace Flovers_WPF
         public Special_Deal_Window()
         {
             InitializeComponent();
+        }
+
+        Camera_Control webcam;
+        List<object> added_images = new List<object>();
+
+        private void spec_deal_Loaded(object sender, RoutedEventArgs e)
+        {
+            webcam = new Camera_Control();
+            webcam.Ini_Web_Camera(ref imgVideo);
+        }
+
+        private void button_startVideo_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Start();
+        }
+
+        private void button_stopVideo_Click(object sender, RoutedEventArgs e)
+        {
+            webcam.Stop();
+        }
+
+        private void button_CaptureImage_Click(object sender, RoutedEventArgs e)
+        {
+            imgCaptured.Source = imgVideo.Source;
+        }
+
+        private void button_SaveImage_Click(object sender, RoutedEventArgs e)
+        {
+            Camera_HelperClass.SaveImageCapture((BitmapSource)imgCaptured.Source);
+        }
+
+        private void add_photo_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog select_pic = new OpenFileDialog();
+            if(select_pic.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                added_images.Add(select_pic.FileName);
+                pictures.Items.Add(select_pic.FileName);
+            }
+        }
+
+        private void delete_photo_Click(object sender, RoutedEventArgs e)
+        {
+            if (pictures.SelectedIndex != -1)
+            {
+                pictures.Items.RemoveAt(pictures.SelectedIndex);
+                added_images.Remove(pictures.SelectedItem);
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("выберите изображение из списка");
+            }
+        }
+
+        private void button_SaveAsPDF_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
