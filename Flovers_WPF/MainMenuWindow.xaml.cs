186043f10e4e32a9a154e8da1d39ae74f8c8a4b2
KnYaZuK - Desktop <knyazuk@yandex.ru>
2015-05-21 16:57:20 +0300

    Переименовал одну форму

--- Flovers_WPF/MainMenuWindow.xaml.cs
+++ Flovers_WPF/MainMenuWindow.xaml.cs
@@ -61,7 +61,7 @@ namespace Flovers_WPF
 
         private void bt_records_Click(object sender, RoutedEventArgs e)
         {
-            Records_goods rec_good = new Records_goods();
+            Components_Window rec_good = new Components_Window();
             rec_good.ShowDialog();
         }
 
